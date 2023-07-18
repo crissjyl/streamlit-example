feeaaf01b2f0ae374fa40b6d5b0d456102f55d3a
Christie Liu <cliu713@gmail.com>
2023-07-17 17:32:00 -0700

    modified:   requirements.txt 	modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from google.oauth2 import service_account
 from google.cloud import bigquery
 # Langchain
 from langchain.callbacks import StreamlitCallbackHandler
-
+from langchain.agents import AgentType, initialize_agent, load_tools
 
 # Vertex AI
 from google.cloud import aiplatform
