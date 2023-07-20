6e512a2059c02b2dc830dcb7427a5c39ef2089e4
Christie Liu <cliu713@gmail.com>
2023-07-20 08:54:42 -0700

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -168,8 +168,8 @@ def load_data(file):
     df_qa = pd.read_csv(file)
     return df_qa
 df_qa = load_data("./data/output2.csv")
-st.dataframe(df_qa, use_container_width=True)
-loader = DataFrameLoader(df_qa, page_content_column="text", hide_index=True)
+st.dataframe(df_qa, use_container_width=True, hide_index=True)
+loader = DataFrameLoader(df_qa, page_content_column="text")
 documents = loader.load()
 
 def splitText(documents):
