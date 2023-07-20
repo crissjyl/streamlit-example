ddfd6d21eb423618d8ec0d4e3eadf24372e46e01
Christie Liu <cliu713@gmail.com>
2023-07-20 08:28:42 -0700

    renamed:    output2.csv -> data/output2.csv 	modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -162,8 +162,8 @@ st.divider()
 st.subheader("Q&A with RetrievalQA Chain")
  
 @st.cache_data(ttl=600)
-df_qa = pd.read_csv('output2.csv')
-st.dataframe(df_qa)
+df_qa = pd.read_csv("./data/output2.csv")
+st.write(df_qa)
 loader = DataFrameLoader(df_qa, page_content_column="text")
 documents = loader.load()
 
